8e51a6e8007f44f3d5a1859a212e4c67d5f3697e
孙连强 <sunlianqiang@bytedance.com>
2025-03-17 17:13:42 +0800

    book res

--- restore_points/restore_point_3_end_chapter_12/alien_invasion.py
+++ restore_points/restore_point_3_end_chapter_12/alien_invasion.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.sprite import Group
 
 from settings import Settings
-from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -14,29 +13,19 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     
-    # Create an instance to store game statistics.
-    stats = GameStats(ai_settings)
-    
     # Set the background color.
     bg_color = (230, 230, 230)
     
-    # Make a ship, a group of bullets, and a group of aliens.
+    # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
     bullets = Group()
-    aliens = Group()
     
-    # Create the fleet of aliens.
-    gf.create_fleet(ai_settings, screen, ship, aliens)
-
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
         
-        if stats.game_active:
-            ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-
 run_game()
